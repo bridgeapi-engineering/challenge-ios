6bcbdf7b60c20ff12c9a7b09f48aac22c46dd0a1
Etienne Jézéquel <etienne.jezequel@icloud.com>
2022-04-27 17:22:04 +0200

    Init project
    Init Xcode project

---
+++ ChallengeiOS/ChallengeiOS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ChallengeiOS
+//
+//  Created by Etienne JEZEQUEL on 27/04/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
